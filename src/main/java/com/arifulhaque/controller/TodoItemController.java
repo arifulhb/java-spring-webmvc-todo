0ebd7979638b3fec08c8e53ee9543f22dc597e07
Ariful Haque <arifulhb@gmail.com>
2021-05-18 23:35:06 +0800

    delete item

--- src/main/java/com/arifulhaque/controller/TodoItemController.java
+++ src/main/java/com/arifulhaque/controller/TodoItemController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 
@@ -58,4 +59,12 @@ public class TodoItemController {
         return "redirect:/" + Mappings.ITEMS;
     }
 
+    @GetMapping(Mappings.DELETE_ITEMS)
+    public String deleteItem(@RequestParam int id) {
+
+        log.info("Deleting item with id={}", id);
+        todoItemService.removeItem(id);
+
+        return "redirect:/" + Mappings.ITEMS;
+    }
 }
